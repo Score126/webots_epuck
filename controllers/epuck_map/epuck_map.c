c6ee47874d02ccdd6a349cb04484627516a3d98a
Score126 <92235002+Score126@users.noreply.github.com>
2021-10-10 18:55:05 -0300

    Update epuck_map.c

--- controllers/epuck_map/epuck_map.c
+++ controllers/epuck_map/epuck_map.c
@@ -1,103 +1,76 @@
 #include <webots/robot.h>
-#include <webots/motor.h>
 #include <webots/distance_sensor.h>
-#include <webots/position_sensor.h>
-#include <webots/supervisor.h>
+#include <webots/motor.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+// time in [ms] of a simulation step
 #define TIME_STEP 64
+
 #define MAX_SPEED 6.28
 
-#define ROBOT_RADIUS 0.035 // meters.
-#define RANGE_MIN 0.005+ROBOT_RADIUS // 0.5 cm + ROBOT_RADIUS.
-#define RANGE_MAX 0.05+ROBOT_RADIUS // 5 cm + ROBOT_RADIUS. 
-
-bool detect_obstacle_ahead(float d[8]) {
-  return ( (d[0] < RANGE_MAX/2.0) || 
-           (d[1] < RANGE_MAX/2.0) || 
-           (d[6] < RANGE_MAX/2.0) || 
-           (d[7] < RANGE_MAX/2.0));
-}
-
-float convert_intensity_to_meters(float prox) {
-  float dist = 0.0;
-  if (prox > 0.0)
-    dist = 0.5/sqrt(prox)+ROBOT_RADIUS;
-  else
-    dist = RANGE_MAX;
-  if (dist > RANGE_MAX)
-    dist = RANGE_MAX;
-  return dist;
-}
- 
-void salvar_posicao_distancias(FILE *log, const double * p, const double * r, float d[8]){
-    fprintf(log, "%f %f %f ", p[0], p[2], (r[1] < 0 ? -r[3] : r[3]) );
-    for(int i = 0; i < 8; i++) {
-      fprintf(log, "%f ", d[i]);
-    }
-    fprintf(log, "\n");
-    fflush(log);
-}
-
-
+// entry point of the controller
 int main(int argc, char **argv) {
-  FILE *log = fopen("log.csv", "w");
-  if (!log)
-    exit(1);
-    
-  ////// EXERCICIO: CRIAR FUNÇÃO PARA SUBSTITUIR ESSE CODIGO USANDO STRUCT /////
+  // initialize the Webots API
   wb_robot_init();
+
+  // internal variables
+  int i;
+  WbDeviceTag ps[8];
+  char ps_names[8][4] = {
+    "ps0", "ps1", "ps2", "ps3",
+    "ps4", "ps5", "ps6", "ps7"
+  };
+
+  // initialize devices
+  for (i = 0; i < 8 ; i++) {
+    ps[i] = wb_robot_get_device(ps_names[i]);
+    wb_distance_sensor_enable(ps[i], TIME_STEP);
+  }
+
   WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
   WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
   wb_motor_set_position(left_motor, INFINITY);
   wb_motor_set_position(right_motor, INFINITY);
-  wb_motor_set_velocity(left_motor, 0.1 * MAX_SPEED);
-  wb_motor_set_velocity(right_motor, 0.1 * MAX_SPEED);
-  WbDeviceTag ps[8];
-  char ps_id[4];
-  for(int i = 0; i < 8; i++){
-    sprintf(ps_id, "ps%d", i);
-    ps[i] = wb_robot_get_device(ps_id);
-    wb_distance_sensor_enable(ps[i], TIME_STEP);
-  }
-  WbNodeRef robot_node = wb_supervisor_node_get_from_def("EPUCK");
-  if (robot_node == NULL) {
-    fprintf(stderr, "No DEF EPUCK node found in the current world file\n");
-    exit(1);
-  }
-  WbFieldRef robot_position = wb_supervisor_node_get_field(robot_node, "translation");
-  WbFieldRef robot_rotation = wb_supervisor_node_get_field(robot_node, "rotation");
-  ////// EXERCICIO: CRIAR FUNÇÃO PARA SUBSTITUIR ESSE CODIGO USANDO STRUCT /////
+  wb_motor_set_velocity(left_motor, 0.0);
+  wb_motor_set_velocity(right_motor, 0.0);
 
+  // feedback loop: step simulation until an exit event is received
   while (wb_robot_step(TIME_STEP) != -1) {
-    const double *position = wb_supervisor_field_get_sf_vec3f(robot_position);
-    const double *rotation = wb_supervisor_field_get_sf_rotation(robot_rotation);
-       
-    float dist[8];
-    for(int i = 0; i < 8; i++) {
-      dist[i] = convert_intensity_to_meters(wb_distance_sensor_get_value(ps[i]));
+    // read sensors outputs
+    double ps_values[8];
+    for (i = 0; i < 8 ; i++)
+      ps_values[i] = wb_distance_sensor_get_value(ps[i]);
+
+    // detect obstacles
+    bool right_obstacle =
+      ps_values[0] > 80.0 ||
+      ps_values[1] > 80.0 ||
+      ps_values[2] > 80.0;
+    bool left_obstacle =
+      ps_values[5] > 80.0 ||
+      ps_values[6] > 80.0 ||
+      ps_values[7] > 80.0;
+
+    // initialize motor speeds at 50% of MAX_SPEED.
+    double left_speed  = 0.5 * MAX_SPEED;
+    double right_speed = 0.5 * MAX_SPEED;
+
+    // modify speeds according to obstacles
+    if (left_obstacle) {
+      // turn right
+      left_speed  += 0.5 * MAX_SPEED;
+      right_speed -= 0.5 * MAX_SPEED;
+    }
+    else if (right_obstacle) {
+      // turn left
+      left_speed  -= 0.5 * MAX_SPEED;
+      right_speed += 0.5 * MAX_SPEED;
     }
 
-    salvar_posicao_distancias(log, position, rotation, dist);
-
-    if ( detect_obstacle_ahead(dist) )
-    {
-      wb_motor_set_velocity(left_motor, 0.2 * MAX_SPEED);
-      wb_motor_set_velocity(right_motor, -0.2 * MAX_SPEED);
-    }
-    if ( !detect_obstacle_ahead(dist) )
-    {
-      wb_motor_set_velocity(left_motor, 0.5 * MAX_SPEED);
-      wb_motor_set_velocity(right_motor, 0.5 * MAX_SPEED);
-    }
+    // write actuators inputs
+    wb_motor_set_velocity(left_motor, left_speed);
+    wb_motor_set_velocity(right_motor, right_speed);
   }
-   
-  fclose(log);
 
+  // cleanup the Webots API
   wb_robot_cleanup();
-
-  return 0;
-}
+  return 0; //EXIT_SUCCESS
